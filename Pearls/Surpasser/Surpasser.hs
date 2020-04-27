ab3bd7d0367206c96c6a08d52fc3d137dcde2e16
Adam Shimi <adam.shimi@toulouse-inp.fr>
2020-04-27 19:03:24 +0200

    Comment divide-and-conquer solution for Surpasser.

--- Pearls/Surpasser/Surpasser.hs
+++ Pearls/Surpasser/Surpasser.hs
@@ -20,6 +20,9 @@ slowSurpasser l = maximum [ (length . filter (> (head) t) . tail) t | t <- tails
 msc :: Ord a => [a] -> Int
 msc = maximum . (map snd) . table
 
+-- Basically implement a merge sort while combining
+-- the counts
+
 table :: Ord a => [a] -> [(a,Int)]
 table [x] = [(x,0)]
 table xs = joinT (m-n) (table ys) (table zs)
@@ -27,9 +30,17 @@ table xs = joinT (m-n) (table ys) (table zs)
                  n = m `div` 2
                  (ys,zs) = splitAt n xs
 
+-- Two important points : txs and tys are sorted (in ascending order
+-- of first element) and all elements in txs appear before
+-- all elements in tys in the original list.
 joinT :: Ord a => Int -> [(a,Int)] -> [(a,Int)] -> [(a,Int)]
 joinT 0 txs [] = txs
 joinT n [] tys = tys
 joinT n txs@((x,c):txs') tys@((y,d):tys')
+  -- if x < y, then all elements of tys are > x (because the lists
+  -- are sorted), and thus nbSurp x = c + length tys = c+n
   | x < y = ((x,c+n):(joinT n txs' tys))
+  -- if x >= y, then we put y in front. And since all elements
+  -- of tys appear before the elements of txs in the original list,
+  -- the count for y is the count in tys, that is d.
   | otherwise = ((y,d):(joinT (n-1) txs tys'))
